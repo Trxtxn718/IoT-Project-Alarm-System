6e04ce5abbb3de8da86341e0a9acacb345be5431
Quax2013 <robin.alex.ernst@gmail.com>
2024-11-24 15:53:24 +0100

    Added: localhost access for website Fixed: Request to server for status

--- Client/script.js
+++ Client/script.js
@@ -1,19 +1,25 @@
 async function toggleAlarm() {
-  let statusElement = document.getElementById("alarm-status")
-  let alarmStatus
-
   try {
-    alarmStatus = await fetch("http://192.168.137.1:3000" + "/notifications/status")
+    let alarmStatus
+    alarmStatus = await fetch("http://127.0.0.1:3000" + "/notifications/status")
+    alarmStatus = await alarmStatus.text()
     console.log("Alarm status:", alarmStatus)
+    updateElement(alarmStatus)
   } catch (error) {
     console.error("Error:", error)
+    upadateElement("UNREACHABLE")
+  }
+}
+
+function updateElement(alarmStatus) {
+  let statusElement = document.getElementById("alarm-status")
+
+  console.log("Alarm status CAHNGER:", alarmStatus)
+  if (alarmStatus === "UNREACHABLE") {
     statusElement.classList.add("off")
     statusElement.innerText = "NOT REACHABLE"
   }
-
-
-
-  if (alarmStatus === "activated") {
+  else if (alarmStatus === "deactivated") {
     statusElement.innerText = "Off"
     statusElement.classList.add("off")
   } else {
@@ -21,3 +27,16 @@ async function toggleAlarm() {
     statusElement.classList.remove("off")
   }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  try {
+    fetch("http://127.0.0.1:3000" + "/notifications/status")
+      .then(response => response.text())
+      .then(alarmStatus => {
+        updateElement(alarmStatus)
+      })
+  }
+  catch (error) {
+    console.error("Error:", error)
+  }
+});
